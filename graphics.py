057fb97aead3d60c22c6505d4e0f98bb43d1faa5
edipretoro <edipretoro@gmail.com>
2025-11-23 11:33:21 +0100

    Iteration for the lesson 5b463508-3371-4df9-8a5c-228431af21b9

--- graphics.py
+++ graphics.py
@@ -1,5 +1,23 @@
 from tkinter import Tk, BOTH, Canvas
 
+
+class Point():
+    def __init__(self, x, y):
+        self.__x = x
+        self.__y = y
+
+
+class Line():
+    def __init__(self, start: Point, end: Point):
+        self.__start = start
+        self.__end = end
+
+    def draw(self, canvas: Canvas, color="black", width=2):
+        canvas.create_line(self.__start._Point__x, self.__start._Point__y,
+                           self.__end._Point__x, self.__end._Point__y,
+                           fill=color, width=width)
+
+
 class Window():
     def __init__(self, width=800, height=600):
         self.__root = Tk()
@@ -22,3 +40,5 @@ class Window():
     def close(self):
         self.__running = False
 
+    def draw_line(self, line: Line, fill_color: str = "black", width: int = 2):
+        line.draw(self.__canvas, color=fill_color, width=width)
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from graphics import Window
+from graphics import Window, Line, Point
 
 
 def main():
     win = Window(800, 600)
+    p1 = Point(100, 100)
+    p2 = Point(700, 500)
+    line = Line(p1, p2)
+    win.draw_line(line, fill_color="blue", width=4)
     win.wait_for_close()
 
 
